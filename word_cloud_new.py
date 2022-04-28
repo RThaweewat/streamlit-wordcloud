afb6ddc436cd0a8f4bf6052ffddbcd36f40977cb
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-04-28 20:18:43 +0700

    Update word_cloud_new.py

--- word_cloud_new.py
+++ word_cloud_new.py
@@ -17,10 +17,14 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 Text = st.text_input('Text :', 'put text here')
 
 st.sidebar.header("Select No. of words you want to display")
-words = st.sidebar.selectbox("No. of words", range(10, 1000, 10))
+words = st.slider('How old are you?', 10, 5000, 100)
+st.write("Maximum words: ", words, 'Characters')
 
 # Create and generate a word cloud image:
-wordcloud = WordCloud(background_color = "white", max_words = words).generate(Text)
+wordcloud = WordCloud(background_color = "white"
+                      , max_words = words
+                      , width=2000
+                      , height=2000).generate(Text)
 
 # Display the generated image:
 plt.imshow(wordcloud, interpolation='bilinear')
