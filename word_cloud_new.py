849ce730d8ab1e69bae55ac907b7af89bfd93139
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-04-28 20:13:49 +0700

    Update word_cloud_new.py

--- word_cloud_new.py
+++ word_cloud_new.py
@@ -14,31 +14,17 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
 # Create some sample text
-URL = st.text_input('URL:', 'put link here')
-st.write('The current link is', URL)
+Text = st.text_input('Text :', 'put text here')
 
 st.sidebar.header("Select No. of words you want to display")
 words = st.sidebar.selectbox("No. of words", range(10, 1000, 10))
 
-if URL is not None:
-    r = requests.get(URL)
-    #using the web scraping library that is Beautiful Soup
-    soup = BeautifulSoup(r.content, 'html.parser')
-    #extracting the data that is in 'div' content of HTML page
-    table = soup.find('div', attrs = {'id':'main-content'})
-    text = table.text
-    #cleaning the data with regular expression library
-    cleaned_text = re.sub('\t', "", text)
-    cleaned_texts = re.split('\n', cleaned_text)
-    cleaned_textss = "".join(cleaned_texts)
-    st.write("Word Cloud Plot")
-    #using stopwords to remove extra words
-    stopwords = set(STOPWORDS)
-    wordcloud = WordCloud(background_color = "white", max_words =
-              words,stopwords = stopwords).generate(cleaned_textss)
+# Create and generate a word cloud image:
+wordcloud = WordCloud(background_color = "white", max_words =
+          words,stopwords = stopwords).generate(Text)
 
 # Display the generated image:
-plt.imshow(wordcloud, interpolation = 'bilinear')
+plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
 plt.show()
 st.pyplot()
