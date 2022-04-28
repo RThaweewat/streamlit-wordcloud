61e438a96562d83e4564c18d5144997bedfcbc70
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-04-28 20:36:58 +0700

    Update word_cloud_new.py

--- word_cloud_new.py
+++ word_cloud_new.py
@@ -1,3 +1,10 @@
+!pip install pythainlp
+!pip install epitran
+!pip install sklearn_crfsuite
+!pip install tensorflow deepcut
+!pip install attacut
+
+
 import streamlit as st
 import pandas as pd
 import requests
