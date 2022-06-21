a64418228db8a34a6aa08fef703b22b8049fa7e2
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-06-22 00:28:29 +0700

    Update boxplot_cust.py

--- boxplot_cust.py
+++ boxplot_cust.py
@@ -4,12 +4,21 @@ import plotly.express as px
 
 df = pd.read_pickle('rfm_results_cleaned.pkl')
 
+
+st.header("RFM Segment in " + segments + " by" + metric)
 clist = df['segment2_lv1'].unique()
 segments = st.selectbox("Select a segment:" ,clist)
 metric = st.selectbox("By Metric:" , ('Recency', 'Monetary', 'Frequency'))
 st.write('Data points:', len(df[df['segment2_lv1'] == segments]))
 
-st.header("RFM Segment in " + segments + " by" + metric)
-fig = px.box(df[df['segment2_lv1'] == segments], x="Segment", y=metric, color="Churn_group")
+FMCG_rfm_level_agg = df[df['segment2_lv1'] == segments].groupby('Segment').agg({
+    'Recency': 'mean',
+    'Frequency': 'mean',
+    'Monetary': ['mean', 'count']
+}).round(1)
+
+st.dataframe(FMCG_rfm_level_agg.style.highlight_max(axis=0))
+
+fig = px.box(df[df['segment2_lv1'] == segments], x="Segment", y=metric, color="Churn_group", boxpoints = False)
 fig.update_traces(quartilemethod="exclusive") 
 st.plotly_chart(fig)
