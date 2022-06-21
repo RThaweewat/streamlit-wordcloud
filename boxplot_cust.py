21650c8ee391ba5a56bdfc578e6beeb997ee2c47
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-06-22 01:19:43 +0700

    Update boxplot_cust.py

--- boxplot_cust.py
+++ boxplot_cust.py
@@ -14,6 +14,7 @@ thaweewr@scg.com
 
 clist = df['segment2_lv1'].unique()
 segments = st.selectbox("Select main segment:" ,clist)
+metric = st.selectbox("By Metric:" , ('Recency', 'Monetary', 'Frequency'))
 st.subheader(f"RFM Segment in {segments} by {metric}")
 st.write('Data points:', len(df[df['segment2_lv1'] == segments]))
 
@@ -36,7 +37,7 @@ fig_1 = px.scatter(df_no_out[df_no_out['segment2_lv1'] == segments], x='Recency'
 
 st.dataframe(test.style.background_gradient(axis=0))
 
-metric = st.selectbox("By Metric:" , ('Recency', 'Monetary', 'Frequency'))
+
 st.subheader(f"RFM Segment in {segments} by {metric}")
 fig = px.box(df_no_out[df_no_out['segment2_lv1'] == segments], x="Segment", y=metric, color="Churn_group", points = False)
 fig.update_traces(quartilemethod="exclusive") 
