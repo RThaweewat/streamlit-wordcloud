9b3f9bd97c52657f63ee8b8783241e7605139b8d
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-06-22 00:57:27 +0700

    Update boxplot_cust.py

--- boxplot_cust.py
+++ boxplot_cust.py
@@ -26,10 +26,13 @@ test = test[['Segment','Count', 'Recency (mean)',
        'Frequency (mean)', 'Monetary (mean)']]
 
 fig_1 = px.scatter(df_no_out[df_no_out['segment2_lv1'] == segments], x='Recency', y='Monetary', color='Segment')
-st.plotly_chart(fig_1)
+
 
 st.dataframe(test.style.background_gradient(axis=0))
 st.subheader(f"RFM Segment in {segments} by {metric}")
 fig = px.box(df_no_out[df_no_out['segment2_lv1'] == segments], x="Segment", y=metric, color="Churn_group", points = False)
 fig.update_traces(quartilemethod="exclusive") 
 st.plotly_chart(fig)
+
+st.subheader(f"{segments} Segment Scatter plot")
+st.plotly_chart(fig_1)
