064723977b8b6ea4a82db2417d347e8574cd5969
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-06-22 01:45:53 +0700

    Update boxplot_cust.py

--- boxplot_cust.py
+++ boxplot_cust.py
@@ -54,10 +54,10 @@ st.dataframe(test.style.background_gradient(axis=0))
 
 
 st.subheader(f"RFM segment in {segments} by {metric}")
-churn_on = st.radio(
-     "Show churn prediction",
-     ('YES', 'NO')
-if churn_on == 'YES':
+
+agree = st.checkbox('I agree')
+
+if agree:
     fig = px.box(df_no_out[df_no_out['segment2_lv1'] == segments], x="Segment", y=metric, color="Churn_group", points = False)
     fig.update_traces(quartilemethod="exclusive") 
     st.plotly_chart(fig)
