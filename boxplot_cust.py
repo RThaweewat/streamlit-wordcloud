1bb3887ffd3af2555056b9addb707c08f4ea1dc1
RThaweewat <104571571+RThaweewat@users.noreply.github.com>
2022-06-22 00:17:02 +0700

    Update boxplot_cust.py

--- boxplot_cust.py
+++ boxplot_cust.py
@@ -4,10 +4,10 @@ import plotly.express as px
 
 df = pd.read_pickle('rfm_results_cleaned.pkl')
 
-clist = df['Segment'].unique()
+clist = df['segment2_lv1'].unique()
 segments = st.sidebar.selectbox("Select a segment:",clist)
 
 st.header("GDP per Capita over time")
-fig = px.box(df, x=segments, y="Recency", color="Churn")
+fig = px.box(df[df['country'] == segments], x=Segment, y="Recency", color="Churn_group")
 fig.update_traces(quartilemethod="exclusive") 
 st.plotly_chart(fig)
